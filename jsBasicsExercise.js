169d398d97778d6ffa4927eeaf143d83b13cb3f4
Serenity Button <serenitybutton@ituness-MacBook-Pro.local>
2020-10-12 14:16:35 -0500

    created jsBasicsExercise file

---
+++ jsBasicsExercise.js
@@ -0,0 +1,29 @@
+console.log(1182);
+
+/*
+Numbers
+Strings
+Booleans
+Null
+Undefined
+*/
+
+/*
+Adding
+Subtracting 
+Multiplying 
+Dividing 
+Modulo
+*/
+// Answer 3
+console.log(3%7); 
+// Answer 1
+console.log(7%3);
+
+console.log(' I\'M MARY POPPINS Y\'ALL!');
+
+console.log("Spider-" + "man");
+
+console.log("Serenity".length + "Button".length);
+
+console.log("Avengers:Endgame"[5]);
